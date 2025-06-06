a6129f5e56f1a45771760c30e0593abbddf65f66
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2025-06-06 17:40:08 +0800

    Initial commit

---
+++ app/src/main/java/com/riprog/toast/MainActivity.java
@@ -0,0 +1,24 @@
+package com.riprog.toast;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String text = getIntent().getStringExtra("toasttext");
+        if (text == null || text.isEmpty()) {
+            text = "RiProG Toast aktif";
+        }
+
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 100);
+        toast.show();
+
+        finish();
+    }
+}
